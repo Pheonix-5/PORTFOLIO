21c0f9cc8fc50aaaddef5b80500b445df983c2af
Phoenix_sharma <82761151+Pheonix-5@users.noreply.github.com>
2023-08-03 09:21:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,32 @@ const navmenu = document.querySelector(".for_low");
 const resume = document.querySelector('.Download_me');
 const resume_btn = document.querySelector('.resume');
 const close_btn = document.querySelector('.close_btn');
+const open_skill = document.querySelector('.skills_arrow');
+const open_exp = document.querySelector('.exp_arrow');
+const skill = document.querySelector('.skill_btn');
+const exp = document.querySelector('.exp_btn');
 
+open_skill.addEventListener("click", () =>{
+    const visibility = skill.getAttribute('data-visible');
+    if(visibility === "false"){
+        skill.setAttribute('data-visible',true);
+    }
+    else if(visibility === "true"){
+        skill.setAttribute('data-visible',false);
+    }
+    
+});
+
+open_exp.addEventListener("click", () =>{
+    const visibility = exp.getAttribute('data-visible');
+    if(visibility === "false"){
+        exp.setAttribute('data-visible',true);
+    }
+    else if(visibility === "true"){
+        exp.setAttribute('data-visible',false);
+    }
+    
+});
 mobilebtn.addEventListener("click", () =>{
     const visibility = navmenu.getAttribute('data-visible');
     if(visibility === "false"){
